a6ae7b4e81b937390a4bb98da2ad8c4102040506
chris <chrishethertonweb@gmail.com>
2022-10-14 13:57:30 +0200

    cleaned up html file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import H1 from './components/H1';
-import SubHeader from './components/SubHeader';
+import H1 from './components/header/H1';
+import SubHeader from './components/header/SubHeader';
+import NumbersSection from './NumbersSection';
 
 function App() {
   return (
     <div className="App">
       <H1 />
       <SubHeader />
+      <NumbersSection />
     </div>
   );
 }
