13dbaaf03938007084bd2ed39dbc121fcebc378a
chris <chrishethertonweb@gmail.com>
2022-10-16 19:38:45 +0200

    cleaned up code

--- src/components/numbers/NumbersSection.js
+++ src/components/numbers/NumbersSection.js
@@ -2,40 +2,34 @@ import { useState } from "react";
 
 function NumbersSection() {
 
-  // const lastNumberArray = []
-  // const firstSixRandomsArray = []
+  const firstFive = [, , , , ,]
+  const lastOne = []
 
-  const [round, setRound] = useState(new Array(5).fill(false))
-  const [lastRound, setLastRound] = useState(new Array(1).fill(false))
+  const [round, setRound] = useState(firstFive)
+  const [lastRound, setLastRound] = useState(lastOne)
 
   const randomNumbers = () => {
 
     round.map((item) =>
       item = Math.floor(Math.random() * 64)
     )
-    // const firstSixRandom = 
-    // console.log(firstSixRandom);
 
     const firstSixRandomsArray = [...round]
-    // const firstSixRandomsArray = [...firstSixRandom]
 
     setRound(firstSixRandomsArray)
 
-
     const lastRandom = Math.floor(Math.random() * 10)
-    const lastNumberArray = [...lastRound, lastRandom]
 
-    setLastRound(lastNumberArray)
+    setLastRound(lastRandom)
 
-    // fullNumbers = round.concat(lastRound)
   }
 
   return (
     <div >
-      <div className="flex justify-center mt-[40px]" >
+      <div className="flex justify-center mt-[40px] mb-[30px]" >
         {
           round.map((item, idx) =>
-            <div key={idx} className='border-3border-solid border-sky-300 w-[30px] h-[30px] rounded-full' >{item}</div>
+            <div key={idx} className='border-3 border-solid border-sky-300 w-[30px] h-[30px] rounded-full' >{item}</div>
           )
         }
         {
