4f761af32f762e97a383b24a3fc04338dd99dd94
chris <chrishethertonweb@gmail.com>
2022-10-16 20:09:07 +0200

    dunno

--- src/components/numbers/NumbersSection.js
+++ src/components/numbers/NumbersSection.js
@@ -8,14 +8,15 @@ function NumbersSection() {
   const [round, setRound] = useState(firstFive)
   const [lastRound, setLastRound] = useState(lastOne)
 
+
   const randomNumbers = () => {
+    console.log('click');
 
     const firstSixRandomsArray = [round.map((item) =>
       item = Math.floor(Math.random() * 64)
     )]
     console.log(firstSixRandomsArray);
 
-    // const firstSixRandomsArray = [...round]
 
     setRound(firstSixRandomsArray)
 
@@ -25,11 +26,11 @@ function NumbersSection() {
     setLastRound(lastRandom)
 
   }
+
   const resetNumbers = () => {
     setRound(firstFive)
     setLastRound(lastOne)
-
-
+    console.log('CLICK');
   }
 
 
@@ -37,17 +38,21 @@ function NumbersSection() {
   return (
     <div >
       <div className="flex justify-center mt-[40px] mb-[30px]" >
-        {
-          round.map((item, idx) =>
-            <div key={idx} className='border-3 border-solid border-sky-300 w-[30px] h-[30px] rounded-full' >{item}</div>
-          )
-        }
-        {
-          lastRound.map((item, idx) =>
-            <div key={idx} className='border-solid border-sky-300 w-[30px] h-[30px] rounded-full ' >{item}</div>
-          )
-          // bg-sky-300
-        }
+        <span>
+          {
+            round.map((item, idx) =>
+              <div key={idx} className='border-3 rounded-full border-solid border-sky-300 w-[30px] h-[30px] mr-[10px]' >{item}</div>
+            )
+          }
+        </span>
+        <span>
+          {
+            // lastRound.map((item, idx) =>
+            <div className='border-solid border-sky-300 bg-sky-300 w-[30px] h-[30px] rounded-full ' >{lastRound}</div>
+            // )
+            // 
+          }
+        </span>
       </div>
       <div>
         <button onClick={resetNumbers} className="border-2 rounded border-teal-600 text-teal-700 w-[70px] h-[45px]">Reset</button>
